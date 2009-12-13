ccf5ba9fb5c0fbf00d6de4a4a81936eea1d0af05
Oliver Charles <oliver.g.charles@googlemail.com>
2009-12-13 20:34:53 +0800

    Pass 100% of tests

--- lib/Perlanet/Entry.pm
+++ lib/Perlanet/Entry.pm
@@ -1,6 +1,17 @@
 package Perlanet::Entry;
 use Moose;
 
+=head1 NAME
+
+Perlanet::Entry - represents an entry in a feed
+
+=head1 DESCRIPTION
+
+This is a wrapper around L<XML::Feed::Entry> with support for linking back to
+the feed from the entry
+
+=cut
+
 has '_entry' => (
   isa => 'XML::Feed::Entry',
   is => 'ro',
--- lib/Perlanet/Feed.pm
+++ lib/Perlanet/Feed.pm
@@ -1,5 +1,12 @@
 package Perlanet::Feed;
 use Moose;
+use XML::Feed;
+
+=head1 NAME
+
+Perlanet::Feed - represents a feed
+
+=cut
 
 has 'title' => (
   isa => 'Str',
@@ -58,6 +65,32 @@ has 'entries' => (
     add_entry => 'push',
   }
 );
+
+=head1 METHODS
+
+=head2 as_xml
+
+Returns a string containing the XML for this feed and all its entries
+
+=cut
+
+sub as_xml {
+  my ($self, $format) = @_;
+
+  my $feed = XML::Feed->new($format);
+  $feed->title($self->title);
+  $feed->link($self->url);
+  $feed->description($self->description);
+  $feed->author($self->author);
+  if ($format eq 'Atom') {
+    $feed->{atom}->author->email($self->email);
+  }
+  $feed->modified($self->modified);
+  $feed->self_link($self->self_link);
+  $feed->id($self->id);
+  $feed->add_entry($_->_entry) for @{ $self->entries };
+  return $feed->as_xml;
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
