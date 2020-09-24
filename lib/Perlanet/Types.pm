234be1f3a791ca167d772103c0b8908e3a1688d8
Dave Cross <dave@dave.org.uk>
2020-09-24 16:31:48 +0100

    Pod for the new class

--- lib/Perlanet/Types.pm
+++ lib/Perlanet/Types.pm
@@ -1,3 +1,18 @@
+=head1 NAME
+
+Perlanet::Types - Various types for the Perlanet system.
+
+=head1 SYNOPSIS
+
+    # n/a
+
+=head1 DESCRIPTION
+
+This class acts as a repository of types used at various places in
+Perlanet.
+
+=cut
+
 package Perlanet::Types;
 
 use Moose::Util::TypeConstraints;
@@ -6,11 +21,17 @@ use DateTime;
 use DateTime::Duration;
 use DateTime::Format::Strptime;
 
+=head1 TYPES
+
+=head2 Perlanet::DateTime
+
+Our subclass of DateTime (along with a coercion to automatically create
+it from a string).
+
+=cut
+
 subtype 'Perlanet::DateTime',
   as 'DateTime';
-
-subtype 'Perlanet::DateTime::Duration',
-  as 'DateTime::Duration';
 
 coerce 'Perlanet::DateTime',
   from 'Str',
@@ -20,10 +41,34 @@ coerce 'Perlanet::DateTime',
     )->parse_datetime($_);
   };
 
+=head2 Perlanet::DateTime::Duration
+
+Our subtype of DateTime::Duration (along with a coercion to automatically
+create it from a hash reference).
+
+=cut
+
+subtype 'Perlanet::DateTime::Duration',
+  as 'DateTime::Duration';
+
 coerce 'Perlanet::DateTime::Duration',
   from 'HashRef',
   via {
     DateTime::Duration->new($_);
   };
+
+=head1 AUTHOR
+
+Dave Cross, <dave@perlhacks.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2020 by Magnum Solutions Ltd.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
 
 1;
